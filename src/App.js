c022da92d90fc3f88372c85005046af75f085076
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-06-19 01:04:14 +0200

    Display HomePage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from './HomePage';
+
 function App() {
   return (
-    <div className="App">
-      Hello World
+    <div className='App'>
+      <HomePage />
     </div>
   );
 }
