ab6f641c4dd36a2a02524fd0c20a8b8849021e23
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-06-19 15:18:54 +0200

    Split HomePage into smaller, specific components

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Directory from '../../components/Directory/Directory';
+
+import './HomePage.scss';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <h1>Welcome to the Homepage</h1>
+    <Directory />
+  </div>
+)
+
+export default HomePage;
