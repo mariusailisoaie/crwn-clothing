ee3b889a0b7d9d53e0ecff45e61c9bcd0b032f46
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-06-20 11:32:32 +0200

    Remove HatsPage. Use ShopPage instead

--- src/App.js
+++ src/App.js
@@ -3,19 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/HomePage/HomePage';
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-)
+import Shop from './components/Shop/Shop';
 
 function App() {
   return (
     <div className='App'>
       <Switch>
         <Route exact path='/' component={ HomePage } />
-        <Route path='/hats' component={ HatsPage } />
+        <Route path='/shop' component={ Shop } />
       </Switch>
     </div>
   );
