b36e0db76f6fdb7d67288611df61f0804fd25bcc
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-06-24 12:46:16 +0200

    Add SignIn component and use FormInput component instead of html input

---
+++ src/components/SignIn/SignIn.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+
+import './SignIn.scss';
+
+import FormInput from '../FormInput/FormInput';
+
+class SignIn extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+    }
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({ email: '', password: '' });
+  }
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    return (
+      <div className='sign-in'>
+        <h2>I already have an account</h2>
+        <span>Sign in with email and password instead</span>
+
+        <form onSubmit={ this.handleSubmit }>
+          <FormInput
+            type='email'
+            name='email'
+            value={ this.state.email }
+            handleChange={ this.handleChange }
+            label='Email'
+            required
+          />
+
+          <FormInput
+            type='password'
+            name='password'
+            value={ this.state.password }
+            handleChange={ this.handleChange }
+            label='Password'
+            required
+          />
+
+          <input type='submit' value='Submit Form' />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default SignIn;
