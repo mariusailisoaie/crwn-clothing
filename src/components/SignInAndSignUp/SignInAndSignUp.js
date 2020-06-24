ef0518907aef033a5e6e936c04e5b1e10c7b12cf
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-06-24 12:46:52 +0200

    Import and use SignIn component

--- src/components/SignInAndSignUp/SignInAndSignUp.js
+++ src/components/SignInAndSignUp/SignInAndSignUp.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import SignIn from '../SignIn/SignIn';
+
 import './SignInAndSignUp.scss';
 
 const SignInAndSignUp = () => (
-  <div className='signin-and-signup'>SIGN IN</div>
-)
+  <div className="signin-and-signup">
+    <SignIn />
+  </div>
+);
 
 export default SignInAndSignUp;
