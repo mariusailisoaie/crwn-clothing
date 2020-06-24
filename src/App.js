a5073d7e266e66231c6161ff4b843969c0fa014d
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-06-25 00:24:45 +0200

    Close auth subscription on componentWillUnmount

--- src/App.js
+++ src/App.js
@@ -17,12 +17,18 @@ class App extends Component {
     }
   }
 
+  unsubscribeFromAuth = null;
+
   componentDidMount() {
-    auth.onAuthStateChanged(user => {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
       this.setState({ currentUser: user });
 
       console.log(user);
     });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
   }
 
   render() {
