f9fd2c7a4027f8b31cc20fda0afa735398817374
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-06-24 09:52:52 +0200

    Use Header component in App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
+import Header from './components/Header/Header';
 import HomePage from './pages/HomePage/HomePage';
 import Shop from './components/Shop/Shop';
 
 function App() {
   return (
     <div className='App'>
+      <Header />
       <Switch>
         <Route exact path='/' component={ HomePage } />
         <Route path='/shop' component={ Shop } />
