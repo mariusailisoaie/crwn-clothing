703c55b75d10999f8fc18ce83c7e25ed908ddedf
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-06-24 23:59:55 +0200

    Convert App.js to class component because state is needed. Add 'auth' from firebase utils. Get user state from firebase and log it

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
@@ -6,18 +6,37 @@ import Header from './components/Header/Header';
 import HomePage from './pages/HomePage/HomePage';
 import Shop from './components/Shop/Shop';
 import SignInAndSignUp from './pages/SignInAndSignUp/SignInAndSignUp';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div className='App'>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={ HomePage } />
-        <Route path='/shop' component={ Shop } />
-        <Route path='/signIn' component={ SignInAndSignUp } />
-      </Switch>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log(user);
+    });
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={ HomePage } />
+          <Route path='/shop' component={ Shop } />
+          <Route path='/signIn' component={ SignInAndSignUp } />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
