4d5a21ac5f0437ac8056e57ae02b85b4c5774970
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-06-24 10:15:44 +0200

    Add SignInAndSignUp component which will contain SignIn and SignUp pages

---
+++ src/components/SignInAndSignUp/SignInAndSignUp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './SignInAndSignUp.scss';
+
+const SignInAndSignUp = () => (
+  <div className='signin-and-signup'>SIGN IN</div>
+)
+
+export default SignInAndSignUp;
