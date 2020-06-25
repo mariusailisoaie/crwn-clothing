a1dffb3d5c2a877f5a17ae348c28d5c8169230f8
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-06-25 15:32:49 +0200

    Import scss. Import and add SignUp component. Change className

--- src/pages/SignInAndSignUp/SignInAndSignUp.js
+++ src/pages/SignInAndSignUp/SignInAndSignUp.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import './SignInAndSignUp.scss';
+
 import SignIn from '../../components/SignIn/SignIn';
+import SignUp from '../../components/SignUp/SignUp';
 
 const SignInAndSignUp = () => (
-  <div className="signin-and-signup">
+  <div className="signin-and-signup-container">
     <SignIn />
+    <SignUp />
   </div>
 );
 
