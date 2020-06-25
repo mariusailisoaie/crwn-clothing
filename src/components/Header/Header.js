061cd9dacb21d964d9f6f4996b831e230b66f54e
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-06-25 12:18:22 +0200

    Display sign in/sign out button bases on user state

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,14 +4,22 @@ import { ReactComponent as Logo } from '../../assets/original.svg';
 
 import './Header.scss';
 
-const Header = () => (
+import { auth } from '../../firebase/firebase.utils';
+
+const Header = ({ currentUser }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
     </Link>
     <div className='options'>
-      <Link className='option' to='/shop'>Shop</Link>
-      <Link className='option' to='/contact'>Contact</Link>
+      <Link className='option' to='/shop'>SHOP</Link>
+      <Link className='option' to='/contact'>CONTACT</Link>
+      {
+        currentUser ?
+          <div className='option' onClick={ () => auth.signOut() }>SIGN OUT</div>
+          :
+          <Link className='option' to='/signin'>SIGN IN</Link>
+      }
     </div>
   </div>
 )
