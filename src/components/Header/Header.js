4fffb389df8c24dcbb0713f93147adc378865f90
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-06-26 13:49:10 +0200

    Use CartDropdown component in Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,6 +6,7 @@ import { auth } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
 
 import CartIcon from '../CartIcon/CartIcon';
+import CartDropdown from '../CartDropdown/CartDropdown';
 
 const Header = ({ currentUser }) => (
   <div className='header'>
@@ -23,6 +24,7 @@ const Header = ({ currentUser }) => (
       }
       <CartIcon />
     </div>
+    <CartDropdown />
   </div>
 )
 
