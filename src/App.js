ac980f106b4a49d40fd8ed3f37729125085bfdb2
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-06-26 12:21:56 +0200

    Render components based on current user state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 
 import Header from './components/Header/Header';
@@ -44,15 +44,19 @@ class App extends Component {
         <Switch>
           <Route exact path='/' component={ HomePage } />
           <Route path='/shop' component={ Shop } />
-          <Route path='/signIn' component={ SignInAndSignUp } />
+          <Route exact path='/signIn' render={ () => this.props.currentUser ? <Redirect to='/' /> : <SignInAndSignUp /> } />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
