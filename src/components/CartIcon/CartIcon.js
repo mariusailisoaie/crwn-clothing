78dace5308b3ec576f8a7314f6235afac1319174
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-06-26 13:38:59 +0200

    Create CartIcon component

---
+++ src/components/CartIcon/CartIcon.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './CartIcon.scss';
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-icon.svg';
+
+const CartIcon = () => (
+  <div className='cart-icon'>
+    <ShoppingIcon className='shopping-icon' />
+    <span className='item-count'>0</span>
+  </div>
+)
+
+export default CartIcon;
