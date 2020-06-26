5a345f26864e38a921af92303bacbe5e0f923b45
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-06-26 11:14:52 +0200

    Connect Header to redux. Get current user from redux instead of props from App

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,6 +6,8 @@ import './Header.scss';
 
 import { auth } from '../../firebase/firebase.utils';
 
+import { connect } from 'react-redux';
+
 const Header = ({ currentUser }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
@@ -24,4 +26,8 @@ const Header = ({ currentUser }) => (
   </div>
 )
 
-export default Header;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
