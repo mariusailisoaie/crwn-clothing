27ab293958441fbaa3da7d54b7432126033664ff
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-06-26 16:05:03 +0200

    Add 'inverted' class to CustomButton based on inverted prop

--- src/components/CustomButton/CustomButton.js
+++ src/components/CustomButton/CustomButton.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 import './CustomButton.scss';
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
-  <button className={ `${ isGoogleSignIn ? 'google-button' : '' } custom-button` } { ...otherProps }>{ children }</button>
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
+  <button className={ `${ inverted ? 'inverted' : '' } ${ isGoogleSignIn ? 'google-button' : '' } custom-button` } { ...otherProps }>{ children }</button>
 )
 
 export default CustomButton;
