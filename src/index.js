2770af82c904431892c99ed7abb7e343cb3f20e3
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-06-26 11:12:33 +0200

    Provide App access redux store

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <Provider store={ store }>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
