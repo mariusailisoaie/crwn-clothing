bd7ddd06615e7a5a94f6ed3e438f49c8886522b5
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-06-26 14:55:17 +0200

    Get cart status from redux and display/hide CartDropdown based on it

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ import { connect } from 'react-redux';
 import CartIcon from '../CartIcon/CartIcon';
 import CartDropdown from '../CartDropdown/CartDropdown';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
@@ -24,12 +24,15 @@ const Header = ({ currentUser }) => (
       }
       <CartIcon />
     </div>
-    <CartDropdown />
+    {
+      hidden ? null : <CartDropdown />
+    }
   </div>
 )
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 
 export default connect(mapStateToProps)(Header);
