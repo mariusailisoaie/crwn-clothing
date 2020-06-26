fdc161d26c7affb83f94441d2f30b4fee6a91701
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-06-26 14:53:50 +0200

    Connect CartIcon component to redux. Dispatch toggleCart action

--- src/components/CartIcon/CartIcon.js
+++ src/components/CartIcon/CartIcon.js
@@ -3,11 +3,19 @@ import React from 'react';
 import './CartIcon.scss';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-icon.svg';
 
-const CartIcon = () => (
-  <div className='cart-icon'>
+import { toggleCart } from '../../actions/cartActions';
+
+import { connect } from 'react-redux';
+
+const CartIcon = ({ toggleCart }) => (
+  <div className='cart-icon' onClick={ toggleCart }>
     <ShoppingIcon className='shopping-icon' />
     <span className='item-count'>0</span>
   </div>
 )
 
-export default CartIcon;
+const mapDispatchToProps = dispatch => ({
+  toggleCart: () => dispatch(toggleCart()),
+});
+
+export default connect(null, mapDispatchToProps)(CartIcon);
