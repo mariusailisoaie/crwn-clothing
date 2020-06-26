f0354d28eeebcfc9304bcd1c083d5ff7141b05ad
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-06-26 14:52:54 +0200

    Combine cartReducer in rootReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import userReducer from './userReducer';
+import cartReducer from './cartReducer';
 
 export default combineReducers({
   user: userReducer,
+  cart: cartReducer,
 });
