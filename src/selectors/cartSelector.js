dce67b1cb22ab4aa0c9b774e9ab062a32bdc0c1e
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-06-28 00:33:52 +0200

    Create cartSelector

---
+++ src/selectors/cartSelector.js
@@ -0,0 +1,10 @@
+import { createSelector } from 'reselect';
+
+const cartSelector = state => state.cart;
+
+export const cartItemsSelector = createSelector([cartSelector], cart => cart.cartItems);
+
+export const cartItemsCountSelector = createSelector(
+  [cartItemsSelector],
+  cartItems => cartItems.reduce((accumulatedQuantity, currentItem) => accumulatedQuantity + currentItem.quantity, 0)
+);
