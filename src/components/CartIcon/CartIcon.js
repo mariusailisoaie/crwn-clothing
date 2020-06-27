06e806b30a3c8c35833b2d3023d886a5c66482a5
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-06-28 00:36:37 +0200

    Use cartItemsCountSelector to update itemsCount

--- src/components/CartIcon/CartIcon.js
+++ src/components/CartIcon/CartIcon.js
@@ -4,18 +4,23 @@ import './CartIcon.scss';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-icon.svg';
 
 import { toggleCart } from '../../actions/cartActions';
+import { cartItemsCountSelector } from '../../selectors/cartSelector';
 
 import { connect } from 'react-redux';
 
-const CartIcon = ({ toggleCart }) => (
+const CartIcon = ({ toggleCart, itemsCount }) => (
   <div className='cart-icon' onClick={ toggleCart }>
     <ShoppingIcon className='shopping-icon' />
-    <span className='item-count'>0</span>
+    <span className='item-count'>{ itemsCount }</span>
   </div>
 )
+
+const mapStateToProps = state => ({
+  itemsCount: cartItemsCountSelector(state),
+});
 
 const mapDispatchToProps = dispatch => ({
   toggleCart: () => dispatch(toggleCart()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
