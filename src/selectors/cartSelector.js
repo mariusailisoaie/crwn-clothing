8fa5053b3f5bf0cd0c8ce9cb69805632335319c3
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-06-29 16:22:12 +0200

    Add cartTotalSelector which will return the total price for the items in the cart

--- src/selectors/cartSelector.js
+++ src/selectors/cartSelector.js
@@ -13,3 +13,8 @@ export const cartItemsCountSelector = createSelector(
   [cartItemsSelector],
   cartItems => cartItems.reduce((accumulatedQuantity, currentItem) => accumulatedQuantity + currentItem.quantity, 0)
 );
+
+export const cartTotalSelector = createSelector(
+  [cartItemsSelector],
+  cartItems => cartItems.reduce((accumulatedQuantity, currentItem) => accumulatedQuantity + currentItem.quantity * currentItem.price, 0)
+);
