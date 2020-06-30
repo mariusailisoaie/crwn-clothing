3be8f010d2c7f2128fd92708ae93e12757408e7c
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-06-30 14:17:38 +0200

    Create shopSelector. Add collectionsSelector

---
+++ src/selectors/shopSelector.js
@@ -0,0 +1,5 @@
+import { createSelector } from 'reselect';
+
+const shopSelector = state => state.shop;
+
+export const collectionsSelector = createSelector([shopSelector], shop => shop.collections);
