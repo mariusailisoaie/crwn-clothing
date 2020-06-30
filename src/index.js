02921d862978ef5cfd30ba53a914c9a8f87e53b3
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-06-30 12:15:34 +0200

    Add functionality for giving main App access to persisted store

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import './index.css';
 import App from './App';
 
 import { Provider } from 'react-redux';
-import store from './store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './store';
 
 ReactDOM.render(
   <Provider store={ store }>
     <React.StrictMode>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={ persistor }>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </React.StrictMode>
   </Provider>,
