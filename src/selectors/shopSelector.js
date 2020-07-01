a1e6bcb7d30fd605c314086efcba696b85eb85af
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-07-01 12:52:50 +0200

    Add collectionSelector and collectionsPreviewSelector

--- src/selectors/shopSelector.js
+++ src/selectors/shopSelector.js
@@ -3,3 +3,13 @@ import { createSelector } from 'reselect';
 const shopSelector = state => state.shop;
 
 export const collectionsSelector = createSelector([shopSelector], shop => shop.collections);
+
+export const collectionsPreviewSelector = createSelector(
+  [collectionsSelector],
+  collections => Object.keys(collections).map(key => collections[key])
+);
+
+export const collectionSelector = collectionId => createSelector(
+  [collectionsSelector],
+  collections => collections[collectionId]
+);
