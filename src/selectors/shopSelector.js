78ec27ae1537a381afac76d00faa8c6fbfc1d675
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-07-02 14:13:48 +0200

    Memoize function that creates collectionSelector

--- src/selectors/shopSelector.js
+++ src/selectors/shopSelector.js
@@ -1,4 +1,5 @@
 import { createSelector } from 'reselect';
+import memoize from 'lodash.memoize';
 
 const shopSelector = state => state.shop;
 
@@ -9,7 +10,7 @@ export const collectionsPreviewSelector = createSelector(
   collections => Object.keys(collections).map(key => collections[key])
 );
 
-export const collectionSelector = collectionId => createSelector(
+export const collectionSelector = memoize(collectionId => createSelector(
   [collectionsSelector],
   collections => collections[collectionId]
-);
+));
