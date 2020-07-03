9b89799c7afef73c52598f1f0ae06143400b8c3c
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-07-03 18:07:23 +0200

    Add function which converts collections array which comes from the firestore db to an object

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -59,4 +59,23 @@ export const addCollectionsAndDocuments = async (collectionKey, collections) => 
   return await batch.commit();
 }
 
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items,
+    }
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+
+    return accumulator;
+  }, {});
+}
+
 export default firebase;
