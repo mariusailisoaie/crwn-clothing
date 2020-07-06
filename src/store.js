402688330c669adc66a0de54eef1e0f975096b12
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-07-06 21:36:28 +0200

    Remove thunk middleware. Set up saga middleware

--- src/store.js
+++ src/store.js
@@ -1,17 +1,24 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import { persistStore } from 'redux-persist';
-import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+
+import { fetchCollectionsStart } from './sagas/shopSagas';
 
 import rootReducer from './reducers/rootReducer';
 
-const middlewares = [thunk];
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddleware.run(fetchCollectionsStart);
+
 export const persistor = persistStore(store);
 
 export default { store, persistor }
