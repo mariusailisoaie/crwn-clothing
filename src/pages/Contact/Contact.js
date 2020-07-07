7ece03f2382c763d84603fdf13ecd0eabaf65316
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-07-07 03:17:01 +0200

    Use react-load-script to load linkedin script

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import Script from 'react-load-script';
+
 import './Contact.scss';
 
 const Contact = () => (
   <div className='contact-page'>
+    <Script url='https://platform.linkedin.com/badges/js/profile.js' />
+
     <h1 className='get-in-touch'>Let's get in touch!</h1>
 
     <div class='LI-profile-badge' data-version='v1' data-size='medium' data-locale='en_US' data-type='vertical' data-theme='light' data-vanity='marius-ailisoaie'>
