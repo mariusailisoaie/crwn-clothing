8cdff29b8451ec67420566521efa8aecac750753
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-07-07 02:49:30 +0200

    Update h1. Add LinkedIn badge

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
+import './Contact.scss';
+
 const Contact = () => (
   <div className='contact-page'>
-    <h1 className='title'>Want to get in touch?</h1>
+    <h1 className='get-in-touch'>Let's get in touch!</h1>
+
+    <div class='LI-profile-badge' data-version='v1' data-size='medium' data-locale='en_US' data-type='vertical' data-theme='light' data-vanity='marius-ailisoaie'>
+      <a class='LI-simple-link' href='https://dk.linkedin.com/in/marius-ailisoaie?trk=profile-badge'>Marius Ailisoaie</a>
+    </div>
   </div>
 )
 
