b15d2b05433c191dfa322118924679819fbd3ef8
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-07-07 15:40:09 +0200

    Create LinkedIn card for contact page

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,17 +1,25 @@
 import React from 'react';
 
-import Script from 'react-load-script';
-
 import './Contact.scss';
 
 const Contact = () => (
   <div className='contact-page'>
-    <Script url='https://platform.linkedin.com/badges/js/profile.js' />
-
     <h1 className='get-in-touch'>Let's get in touch!</h1>
 
-    <div class='LI-profile-badge' data-version='v1' data-size='medium' data-locale='en_US' data-type='vertical' data-theme='light' data-vanity='marius-ailisoaie'>
-      <a class='LI-simple-link' href='https://dk.linkedin.com/in/marius-ailisoaie?trk=profile-badge'>Marius Ailisoaie</a>
+    <div className='linkedin-card'>
+      <div className='linkedin-card-top'>
+        <img className='linkedin-background' src='https://static-exp1.licdn.com/sc/h/856xpihrituhwdjrua9z5u5na' alt='linkedin' />
+        <img className='profile-picture' src='https://i.ibb.co/0QX0pVk/0.jpg' alt='profile' />
+      </div>
+
+      <div className='linkedin-card-bottom'>
+        <a className='name' href='https://www.linkedin.com/in/marius-ailisoaie/'>Marius Ailisoaie</a>
+        <span className='position'>Software Developer</span>
+        <a className='profile-button' href='https://www.linkedin.com/in/marius-ailisoaie/'>View profile</a>
+        <a className='linkedin-logo' href='https://www.linkedin.com/in/marius-ailisoaie/'>
+          <img src='https://static-exp1.licdn.com/scds/common/u/images/logos/linkedin/logo_linkedin_93x21_v2.png' alt='linkedin-icon' srcset='' />
+        </a>
+      </div>
     </div>
   </div>
 )
