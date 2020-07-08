38db10e108d6cddb50d2dd9ccff83be2c5529b21
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-07-09 00:45:40 +0200

    Use refactored userActions instead of the old ones. Refactor sagas and reuse code

--- src/sagas/userSagas.js
+++ src/sagas/userSagas.js
@@ -2,18 +2,26 @@ import { takeLatest, put, all, call } from 'redux-saga/effects';
 
 import UserActionTypes from '../actions/actionTypes/userActionTypes';
 
-import { googleSignInSuccess, googleSignInFailure, emailSignInSuccess, emailSignInFailure } from '../actions/userActions';
+import { signInSuccess, signInFailure } from '../actions/userActions';
 
 import { auth, googleProvider, createUserProfileDocument } from '../firebase/firebase.utils';
+
+export function* getSnapshotFromUserAuth(userAuth) {
+  try {
+    const userRef = yield call(createUserProfileDocument, userAuth);
+    const userSnapshot = yield userRef.get();
+    yield put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }));
+  } catch (error) {
+    yield put(signInFailure(error));
+  }
+}
 
 export function* signInWIthGoogle() {
   try {
     const { user } = yield auth.signInWithPopup(googleProvider);
-    const userRef = yield call(createUserProfileDocument, user);
-    const userSnapshot = yield userRef.get();
-    yield put(googleSignInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }));
+    yield getSnapshotFromUserAuth(user);
   } catch (error) {
-    yield put(googleSignInFailure(error));
+    yield put(signInFailure(error));
   }
 }
 
@@ -24,11 +32,9 @@ export function* onGoogleSignInStart() {
 export function* signInWithEmail({ payload: { email, password } }) {
   try {
     const { user } = yield auth.signInWithEmailAndPassword(email, password);
-    const userRef = yield call(createUserProfileDocument, user);
-    const userSnapshot = yield userRef.get();
-    yield put(emailSignInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }));
+    yield getSnapshotFromUserAuth(user);
   } catch (error) {
-    yield put(emailSignInFailure(error));
+    yield put(signInFailure(error));
   }
 }
 
