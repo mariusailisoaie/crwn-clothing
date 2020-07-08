7914ee9d67194e6f14dddc240abe0eae57ae5b50
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-07-08 23:02:03 +0200

    Add error field to initial state. Replace SET_CURRENT_USER action with GOOGLE_SIGN_IN_SUCCESS. Add another case for GOOGLE_SIGN_IN_FAILURE

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,15 +1,22 @@
 import UserActionTypes from '../actions/actionTypes/userActionTypes';
 
 const INITIAL_STATE = {
-  currentUser: null
+  currentUser: null,
+  error: '',
 }
 
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case UserActionTypes.SET_CURRENT_USER:
+    case UserActionTypes.GOOGLE_SIGN_IN_SUCCESS:
       return {
         ...state,
         currentUser: action.payload,
+        error: '',
+      }
+    case UserActionTypes.GOOGLE_SIGN_IN_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
       }
     default:
       return state;
