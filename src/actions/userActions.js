3dff78f21ada171ae283bacc995b5a08994567cd
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-07-08 17:39:58 +0200

    Add actions for google sign in

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -4,3 +4,17 @@ export const setCurrentUser = user => ({
   type: UserActionTypes.SET_CURRENT_USER,
   payload: user
 });
+
+export const googleSignInStart = () => ({
+  type: UserActionTypes.GOOGLE_SIGN_IN_START,
+});
+
+export const googleSignInSuccess = user => ({
+  type: UserActionTypes.GOOGLE_SIGN_IN_SUCCESS,
+  payload: user,
+});
+
+export const googleSignInFailure = error => ({
+  type: UserActionTypes.GOOGLE_SIGN_IN_FAILURE,
+  payload: error,
+});
