6df22f80981a96c73ce69a0bf4256eab2dfa32cc
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-07-08 23:01:57 +0200

    Add userSagas in rootSaga

--- src/sagas/rootSaga.js
+++ src/sagas/rootSaga.js
@@ -1,9 +1,11 @@
 import { all, call } from 'redux-saga/effects';
 
 import { fetchCollectionsStart } from './shopSagas';
+import { userSagas } from './userSagas';
 
 export default function* rootSaga() {
   yield all([
     call(fetchCollectionsStart),
+    call(userSagas),
   ]);
 }
