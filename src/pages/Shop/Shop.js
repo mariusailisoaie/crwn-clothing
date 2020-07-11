2e1662d0072591f29e13feaf51132de1d40d26ef
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-07-11 14:51:00 +0200

    Update Shop page to use hooks instead of lifecycle methods

--- src/pages/Shop/Shop.js
+++ src/pages/Shop/Shop.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { Route } from 'react-router-dom';
 
 import CollectionsOverviewContainer from '../../containers/CollectionsOverviewContainer';
@@ -7,22 +7,17 @@ import CollectionContainer from '../../containers/CollectionContainer';
 import { connect } from 'react-redux';
 import { fetchCollectionsStart } from '../../actions/shopActions';
 
-class ShopPage extends Component {
-  componentDidMount() {
-    const { fetchCollectionsStart } = this.props;
-    fetchCollectionsStart();
-  }
+const ShopPage = ({ fetchCollectionsStart, match }) => {
+  useEffect(() => {
+    fetchCollectionsStart()
+  }, [fetchCollectionsStart]);
 
-  render() {
-    const { match } = this.props;
-
-    return (
-      <div className='shop-page'>
-        <Route exact path={ `${ match.path }` } component={ CollectionsOverviewContainer } />
-        <Route path={ `${ match.path }/:collectionId` } component={ CollectionContainer } />
-      </div>
-    )
-  }
+  return (
+    <div className='shop-page'>
+      <Route exact path={ `${ match.path }` } component={ CollectionsOverviewContainer } />
+      <Route path={ `${ match.path }/:collectionId` } component={ CollectionContainer } />
+    </div>
+  )
 }
 
 const mapDispatchToProps = dispatch => ({
