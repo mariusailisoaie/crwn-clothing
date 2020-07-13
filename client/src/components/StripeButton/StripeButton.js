2a96d6452cab6bee5a3779d792bcd96d836eb846
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-07-13 04:54:19 +0200

    Add FE functionality for creating stripe charges. Add notifications

--- client/src/components/StripeButton/StripeButton.js
+++ client/src/components/StripeButton/StripeButton.js
@@ -2,13 +2,32 @@ import React from 'react';
 
 import StripeCheckout from 'react-stripe-checkout';
 
+import addNotification from '../../utils/notifications.utils';
+
+import axios from 'axios';
+
 const StripeButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_4gu0fGuFiI84EiPVCku1iKQK';
 
   const onToken = token => {
-    console.log('log: StripeButton -> token', token);
-    alert('Payment successful');
+    axios.post('payments', { token, amount: priceForStripe })
+      .then(res => {
+        addNotification('Success!', 'Your payment was successful!', 'success', 'top', 'top-center', 'fadeIn', 'fadeOut', 5000, true);
+      }).catch(err => {
+        addNotification(
+          'Payment error!',
+          'There was an issue with your payment! Please make sure you use the provided credit card.',
+          'danger',
+          'top',
+          'top-center',
+          'headShake',
+          'fadeOut',
+          5500,
+          true,
+        );
+        console.log(err)
+      });
   }
 
   return (
