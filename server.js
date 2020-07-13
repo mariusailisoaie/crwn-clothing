503f8c93a7a07dd68b2b928c95e80faaa4d9f0fd
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-07-13 04:52:01 +0200

    Add BE route for creating stripe charges. Add functionality for serving the client production build

--- server.js
+++ server.js
@@ -5,5 +5,42 @@ const path = require('path');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+app.post('/payments', (req, res) => {
+  const { token, amount } = req.body;
+
+  const body = {
+    source: token.id,
+    amount,
+    currency: 'dkk',
+  }
+
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).json({ error: stripeErr });
+    } else {
+      res.status(200).json({ success: stripeRes });
+    }
+  });
+});
+
+app.listen(port, error => {
+  if (error) throw error;
+  console.log(`Server running on port ${ port }`);
+});
