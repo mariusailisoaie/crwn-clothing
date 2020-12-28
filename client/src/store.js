d2210f79308f1861f088a56fa740d9b2300e8075
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-12-28 11:00:55 +0100

    Connect app to redux devTools

--- client/src/store.js
+++ client/src/store.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import { persistStore } from 'redux-persist';
 import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './reducers/rootReducer';
 
@@ -15,7 +16,7 @@ if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
 }
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)));
 
 sagaMiddleware.run(rootSaga);
 
