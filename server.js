da809da91fcb3f920bd7cc91568ec16b642a8804
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2021-01-08 18:02:22 +0100

    Add route that can be pinged for the app stay alive. Saving file removed semicolons

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const path = require('path')
 
-if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
-const app = express();
-const port = process.env.PORT || 5000;
+const app = express()
+const port = process.env.PORT || 5000
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'client/build')))
 
   app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+  })
 }
 
 app.post('/payments', (req, res) => {
-  const { token, amount } = req.body;
+  const { token, amount } = req.body
 
   const body = {
     source: token.id,
@@ -33,14 +33,18 @@ app.post('/payments', (req, res) => {
 
   stripe.charges.create(body, (stripeErr, stripeRes) => {
     if (stripeErr) {
-      res.status(500).json({ error: stripeErr });
+      res.status(500).json({ error: stripeErr })
     } else {
-      res.status(200).json({ success: stripeRes });
+      res.status(200).json({ success: stripeRes })
     }
-  });
-});
+  })
+})
+
+app.get('/ping', (req, res) => {
+  res.json({ 'message': 'pong' })
+})
 
 app.listen(port, error => {
-  if (error) throw error;
-  console.log(`Server running on port ${ port }`);
-});
+  if (error) throw error
+  console.log(`Server running on port ${ port }`)
+})
